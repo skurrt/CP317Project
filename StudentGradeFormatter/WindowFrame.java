793ac34711e1660b9de01540c0b62bf017a2ea06
Robert Glennie <rg19283746@gmail.com>
2025-07-21 23:07:17 -0400

    Created WindowFrame Class File

---
+++ StudentGradeFormatter/WindowFrame.java
@@ -0,0 +1,16 @@
+package StudentGradeFormatter;
+
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class WindowFrame {
+    
+
+    
+}
