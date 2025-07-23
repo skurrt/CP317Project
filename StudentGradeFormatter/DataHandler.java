8b3f0eac9d6a6237b96e15c2c1492db70e57d736
Robert Glennie <rg19283746@gmail.com>
2025-07-22 21:45:50 -0400

    datahandler class created

---
+++ StudentGradeFormatter/DataHandler.java
@@ -0,0 +1,11 @@
+package StudentGradeFormatter;
+
+import java.util.TreeMap;
+
+public class DataHandler {
+    
+    static TreeMap<Integer, Student> students = new TreeMap<>();
+
+    
+
+}
--- StudentGradeFormatter/Main.java
+++ StudentGradeFormatter/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        DataHandler.addVals();
+
         WindowFrame window = new WindowFrame();
     }
     
